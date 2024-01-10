6bdd0e1a4343db5346779a536d56f9d2d53096b0
Anuj Kumar <anujk7.dev@gmail.com>
2024-01-10 11:06:59 +0530

    committed

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,18 +1,42 @@
-import styles from '@/app/ui/dashboard/users/singleUser/singleUser.module.css'
-import Image from 'next/image'
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+import Image from "next/image";
 
 const SingleUserPage = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.infoContainer}>
-                <div className={styles.imgContainer}>
-                    <Image src="/noavatar.png" alt='' fill/>
-                </div>
-                Anuj Kumar
-            </div>
-            <div className={styles.formContainer}></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
         </div>
-    )
-}
+        Anuj Kumar
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Anuj Kumar" />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="anujk7.dev@gmail.com" />
+          <label>Password</label>
+          <input type="password" name="password"/>
+          <label>Phone</label>
+          <input type="text" name="phone" placeholder="+1234567" />
+          <label>Address</label>
+          <input type="text" name="address" placeholder="New York" />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default SingleUserPage
+export default SingleUserPage;
