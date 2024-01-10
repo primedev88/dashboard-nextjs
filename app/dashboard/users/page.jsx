08dc6131e6f58e38a53f003b24ae40c0b38fe65c
Anuj Kumar <anujk7.dev@gmail.com>
2024-01-10 19:13:04 +0530

    committed

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -3,8 +3,11 @@ import styles from "@/app/ui/dashboard/users/users.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import Pagination from "../pagination/pagination";
+import { fetchUsers } from "@/app/lib/data";
 
-const UsersPage = () => {
+const UsersPage = async ({searchParams}) => {
+  const q = searchParams?.q||"";
+  const users = await fetchUsers(q);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -25,26 +28,27 @@ const UsersPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {users.map((user) => (
+          <tr key={user.id}>
             <td>
               <div className={styles.user}>
                 <Image
-                  src="/noavatar.png"
+                  src={user.img || "/noavatar.png"}
                   alt=""
                   width={40}
                   height={40}
                   className={styles.userImage}
                 />
-                Anuj Kumar
+                {user.username}
               </div>
             </td>
-            <td>anujk7.dev@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>active</td>
+            <td>{user.email}</td>
+            <td>{user.createdAt?.toString().slice(4,16)}</td>
+            <td>{user.isAdmin ? "Admin":"Client"}</td>
+            <td>{user.isActive ? "active":"passive"}</td>
             <td>
               <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
+                <Link href={`/dashboard/users/${user.id}`}>
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
@@ -55,6 +59,7 @@ const UsersPage = () => {
               </div>
             </td>
           </tr>
+          ))}
         </tbody>
       </table>
       <Pagination/>
